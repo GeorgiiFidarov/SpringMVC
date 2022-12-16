77f849a64f976ca991143819e42f02b1d6b6856a
Georgii <fidarov371@gmail.com>
2022-12-17 00:37:26 +0300

    Initial commit

--- src/main/java/ru/fidarov/SpringMVC/controllers/FirstController.java
+++ src/main/java/ru/fidarov/SpringMVC/controllers/FirstController.java
@@ -3,8 +3,10 @@ package ru.fidarov.SpringMVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(){
